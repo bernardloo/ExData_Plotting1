2dfbda5745ceecd15014f9618a129c058f2dfbf6
bernardloo <bernardloo@yahoo.com.sg>
2015-04-10 21:37:22 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Author: Bernard Loo
+######################
+
+setwd("c:/A_R")
+
+source("load_data.R")
+
+plot1 <- paste(getwd(), "plot1.png", sep = "")
+
+	png("plot1.png", width = 480, height = 480)
+	hist(cargardatos$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+	dev.off()
