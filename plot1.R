58df2a65c4aa0bdc92959d6ee081c9ada99be080
bernardloo <bernardloo@yahoo.com.sg>
2015-04-11 14:22:29 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,10 @@
 # Bernard Loo
 ######################
 
-setwd("c:/A_R")
+setwd("c:/A_R/4 ExpDA")
 
 source("load_data.R")
 
-plot1 <- paste(getwd(), "plot1.png", sep = "")
-
-	png("plot1.png", width = 480, height = 480)
-	hist(cargardatos$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
-	dev.off()
+png("plot1.png", width = 480, height = 480)
+hist(hpc$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+dev.off()
