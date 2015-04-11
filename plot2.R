98a4ca452494f1640cd2f15fba9328893f00af20
bernardloo <bernardloo@yahoo.com.sg>
2015-04-11 14:35:18 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,10 @@
+# Bernard Loo
+######################
 
+setwd("c:/A_R/4 ExpDA")
+
+source("load_data.R")
+
+png("plot2.png", width = 480, height = 480)
+plot(hpc$Time, hpc$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
