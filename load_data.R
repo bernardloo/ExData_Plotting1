443f2bba3614e8196db2fee6099127ad8ce70c8a
bernardloo <bernardloo@yahoo.com.sg>
2015-04-11 12:22:14 +0800

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -1,39 +1,23 @@
-# Bernard Loo
+# Bernard Loo 
+# to extract only 2007-02-01 and 2007-02-02 and save it 
+# as an RDS file in prep for drawing graphs
 ###########################
 
-setwd("c:/A_R")
+setwd("c:/A_R/4 ExpDA")
 
 library(httr) 
 
-direccion <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-datos <- "datos"
-if(!file.exists(datos)){
-	dir.create(datos)
-} 
-graficos <- "graficos" 
-if(!file.exists(graficos)){
-	dir.create(graficos)
-}
-archivo <- paste(getwd(), "/datos/household_power_consumption.zip", sep = "")
-if(!file.exists(archivo)){
-	download.file(direccion, archivo, method="curl", mode="wb")
-}
-archivo <- paste(getwd(), "/datos/household_power_consumption.txt", sep = "")
-if(!file.exists(archivo)){
-	unzip(archivo, list = FALSE, overwrite = FALSE, exdir = datos)
-}
+newData <- paste(getwd(), "/newData.rds", sep = "")
 
-datosresumidos <- paste(getwd(), "/datos/datosresumidos.rds", sep = "")
-if(!file.exists(datosresumidos)){
-	datos <- "datos/household_power_consumption.txt"
-	cargardatos <- read.table(datos, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?")
-	cargardatos$Time <- strptime(paste(cargardatos$Date, cargardatos$Time), "%d/%m/%Y %H:%M:%S")
-	cargardatos$Date <- as.Date(cargardatos$Date, "%d/%m/%Y")
-	fechas <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
-	cargardatos <- subset(cargardatos, Date %in% fechas)
-	datosresumidos <- paste(getwd(), "/", "datos", "/", "datosresumidos", ".rds", sep="")
-	saveRDS(cargardatos, datosresumidos)
-} else {
-	datos <- "datos/datosresumidos.rds"
-	cargardatos <- readRDS(datos)
-}
+    rawData <- "household_power_consumption.txt"
+    hpc <- read.table(rawData, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?")
+    hpc$Time <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
+    hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+    
+    #extract only 2007-02-01 and 2007-02-02
+
+    FixPeriod <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d") 
+    hpc <- subset(hpc, Date %in% FixPeriod)
+
+    saveRDS(hpc, newData)
+
